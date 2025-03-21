7ec5b5a343a5e000c393c87ff89dbc83b7085205
Valentin_Kh <valentin_kh@outlook.fr>
2025-03-21 13:38:01 +0100

    Correct echo -e and files mv

--- run.sh
+++ run.sh
@@ -9,25 +9,25 @@ echo
 [[ ! -d $prismdir ]] && mkdir $prismdir
 [[ ! -d $savedir ]] && mkdir $savedir
 
-echo "\033[32mDownloading gamefiles...\033[0m"
+echo -e "\033[32mDownloading gamefiles...\033[0m"
 if wget https://github.com/MoulinetteMC/MoulinetteMC/archive/refs/heads/main.zip; then 
-   echo "\033[32mDecompressing gamefiles...\033[0m"
+   echo -e "\033[32mDecompressing gamefiles...\033[0m"
    unzip ./main.zip -d "$prismdir/instances/"
-   mv $prismdir/instances/MoulinetteMC-main/ $prismdir/instances/MoulinetteMC/
+   mv -rf $prismdir/instances/MoulinetteMC-main/ $prismdir/instances/MoulinetteMC/
 else 
-   echo "\033[31mFailed to download instance\033[0m"
+   echo -e "\033[31mFailed to download instance\033[0m"
    pause
 fi
 
-echo "\033[32mImport player's settings...\033[0m"
+echo -e "\033[32mImport player's settings...\033[0m"
 [[ -f "$savedir/accounts.json" ]] &&
-   cp $savedir/accounts.json $prismdir
+   cp -f $savedir/accounts.json $prismdir
 
 [[ -f "$savedir/prismlauncher.cfg" ]] && 
-   cp $savedir/prismlauncher.cfg $prismdir
+   cp -f $savedir/prismlauncher.cfg $prismdir
 
 [[ -f "$savedir/options.txt" ]] && 
-   cp $savedir/options.txt \
+   cp -f $savedir/options.txt \
       $prismdir/instances/MoulinetteMC/
 
 [[ -d "$savedir/xaero/minimap/" ]] && 
@@ -42,15 +42,15 @@ echo "\033[32mImport player's settings...\033[0m"
 
 nix-shell -p prismlauncher --command "prismlauncher -l MoulinetteMC -a $account"
 
-echo "\033[32mExport player's settings...\033[0m"
+echo -e "\033[32mExport player's settings...\033[0m"
 [[ -f "$prismdir/accounts.json" ]] &&
-   cp $prismdir/accounts.json $savedir/accounts.json
+   cp -f $prismdir/accounts.json $savedir/accounts.json
 
 [[ -f "$prismdir/prismlauncher.cfg" ]] && 
-   cp $prismdir/prismlauncher.cfg $savedir/prismlauncher.cfg
+   cp -f $prismdir/prismlauncher.cfg $savedir/prismlauncher.cfg
 
 [[ -f "$prismdir/options.txt" ]] && 
-   cp $prismdir/instances/MoulinetteMC/ \
+   cp -f $prismdir/instances/MoulinetteMC/ \
       $savedir/options.txt  
 
 [[ -d "$prismdir/instances/MoulinetteMC/.minecraft/xaero/minimap/Multiplayer_valentinkh.ddns.net/" ]] && 
